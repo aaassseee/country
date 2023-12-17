903752e524f5d4da06159a749b53e63d4ed12b69
Jack Liu <ywp033319@gmail.com>
2023-12-17 15:45:54 +0800

    feat: added flutter_country_utility package to replace flutter_country library

---
+++ flutter_country_utility/lib/flutter_country_utility.dart
@@ -0,0 +1,5 @@
+library flutter_country_utility;
+
+export 'package:country/country.dart';
+
+export 'src/extension.dart';
---
+++ flutter_country_utility/lib/src/extension.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:country/country.dart';
+
+extension CountryExtension on Country {
+  /// Quick way to get country short name by dart:ui locale
+  /// throws [ArgumentError] when could not find any country short name
+  ///
+  /// The default locale is [PlatformDispatcher.instance.locale]
+  String getIsoShortNameByLocale([Locale? locale]) {
+    final shortName = tryGetIsoShortNameByLocale(locale);
+    if (shortName == null) {
+      throw ArgumentError('Invalid locale');
+    }
+
+    return shortName;
+  }
+
+  /// Quick way to get country short name by dart:ui locale
+  ///
+  /// The default locale is [PlatformDispatcher.instance.locale]
+  String? tryGetIsoShortNameByLocale([Locale? locale]) {
+    locale ??= PlatformDispatcher.instance.locale;
+
+    final languageCode = locale.languageCode;
+    final scriptCode = locale.scriptCode;
+    final countryCode = locale.countryCode;
+
+    return isoShortNameByLocale[[
+          languageCode,
+          if (scriptCode != null) scriptCode,
+          if (countryCode != null) countryCode,
+        ].join('_')] ??
+        isoShortNameByLocale[[
+          languageCode,
+          if (countryCode != null) countryCode,
+        ].join('_')] ??
+        isoShortNameByLocale[languageCode];
+  }
+}
